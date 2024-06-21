d1dc0d65cc3abd79d340f89ba59b63cf1523122f
Kauan Bertalha <kauan.hb2004@gmail.com>
2024-06-20 21:38:55 -0300

    Up

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,25 +1,5 @@
-import React from 'react';
-import Head from 'next/head';
-import Header from '../components/Header';
-import CardSection from '../components/CardSection';
-import styles from '../app/page.module.css';
+import App from "./pages/app/page";
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Squirtle</title>
-                <meta name="description" content="conteudo" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <Header titulo="Squirtle" />
-
-            <main className={styles.main}>
-                <CardSection titulo="Seção 1">
-                    <p>Teste</p>
-                </CardSection>
-            </main>
-        </div>
-    );
+  return <App />;
 }
